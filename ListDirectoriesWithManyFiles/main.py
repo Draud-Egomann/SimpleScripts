d51ebc64531d0ed43b225f171d13a53821b9ad96
Justin <justin-urbanek@bluewin.ch>
2024-03-27 19:01:12 +0100

    refactor: outsource listing of folders into function

--- ListDirectoriesWithManyFiles/main.py
+++ ListDirectoriesWithManyFiles/main.py
@@ -31,17 +31,7 @@ def remove_folders(found_paths, selection):
                     except Exception as e:
                         print(f"Error when removing folder: {e}")
 
-# Mock array of folder names (replace with actual folder names as needed)
-mock_array = ["node_modules", "vendor", ".nuxt"]
-found_paths = []
-
-# Replace 'root_directory_path' with the path of the root directory you want to scan
-root_directory_path = "C:/Users/Justin/Documents/pytest"
-
-if __name__ == "__main__":
-    scan_folders(root_directory_path, mock_array, found_paths)
-    
-    # Printing all found paths
+def list_folders(found_paths):
     printed_folders = set()  # Set to keep track of printed folder names
     for item in found_paths:
         for folder_name, paths in item.items():
@@ -54,26 +44,20 @@ if __name__ == "__main__":
                 for path in paths:
                     print(f"   {path}")
 
+# Mock array of folder names (replace with actual folder names as needed)
+mock_array = ["node_modules", "vendor", ".nuxt"]
+found_paths = []
+
+# Replace 'root_directory_path' with the path of the root directory you want to scan
+root_directory_path = "C:/Users/Justin/Documents/pytest"
+
+if __name__ == "__main__":
+    scan_folders(root_directory_path, mock_array, found_paths)
+    
+    # Printing all found paths
+    list_folders(found_paths)
+
     print("\n")
     response = input("Do you want to remove any of the found folders? (y/n): ")
     if response.lower() == "y":
-        print(found_paths)
-
-        # print("0: Select all")
-        # for i, item in enumerate(found_paths):
-        #     for folder_name, paths in item.items():
-        #         for path in paths:
-        #             print(f"{i+1}: {folder_name}")
-        # try:
-        #     selection = input("Your selection: ")
-        #     selection = int(selection) # convert input to integer
-        # except ValueError:
-        #     print("Invalid input. Exiting program...")
-        #     input("Press any key to exit...")
-        #     exit()
-
-        remove_folders(found_paths, selection)
-
-    # temporary disable for testing
-    # print("Program execution completed.")
-    # input("Press any key to exit...")
+        remove_folders(found_paths)
