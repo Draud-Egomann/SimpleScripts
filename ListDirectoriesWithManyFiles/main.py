b0f1cf7a8f42bef7c4875aa17c8200d187b07f74
Justin <justin-urbanek@bluewin.ch>
2024-03-27 18:47:55 +0100

    fix: append path with same folder name to one entry instead of creating a new one

--- ListDirectoriesWithManyFiles/main.py
+++ ListDirectoriesWithManyFiles/main.py
@@ -6,10 +6,21 @@ def scan_folders(root_dir, folder_names, found_paths):
         for folder_name in folder_names:
             if folder_name in dirs:
                 folder_path = os.path.join(root, folder_name)
-                found_paths.append({folder_name: [folder_path]})
+
+                found = False
+                # Check if folder is already in found_paths
+                for item in found_paths:
+                    if folder_name in item:
+                        # Append path to existing folder_name key
+                        item[folder_name].append(folder_path)
+                        found = True
+                        break
+                if not found:
+                    # Add new folder_name key to found_paths
+                    found_paths.append({folder_name: [folder_path]})
                 dirs.remove(folder_name)  # Exclude found folder from further search
 
-def remove_folders(found_paths):
+def remove_folders(found_paths, selection):
     for item in found_paths:
         for folder_name, paths in item.items():
             for path in paths:
@@ -46,7 +57,22 @@ if __name__ == "__main__":
     print("\n")
     response = input("Do you want to remove any of the found folders? (y/n): ")
     if response.lower() == "y":
-        remove_folders(found_paths)
+        print(found_paths)
+
+        # print("0: Select all")
+        # for i, item in enumerate(found_paths):
+        #     for folder_name, paths in item.items():
+        #         for path in paths:
+        #             print(f"{i+1}: {folder_name}")
+        # try:
+        #     selection = input("Your selection: ")
+        #     selection = int(selection) # convert input to integer
+        # except ValueError:
+        #     print("Invalid input. Exiting program...")
+        #     input("Press any key to exit...")
+        #     exit()
+
+        remove_folders(found_paths, selection)
 
     # temporary disable for testing
     # print("Program execution completed.")
